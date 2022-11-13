c57e1dc60709b2cdfeef52f8bd299f2329977484
Mark Muigai <markmuigai@gmail.com>
2022-11-13 18:35:44 +0300

    print

--- life_expectancy.py
+++ life_expectancy.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 # from sklearn.model_selection import trai
 
 dataset = pd.read_csv('life_exp.csv');
-print()
+dataset.drop('Country', inplace=True, axis=1)
+dataset.drop('Status', inplace=True, axis=1)
 
-#correlation = np.corrcoef(dataset);
-#print(correlation);
+var =np.corrcoef(dataset)
+print(var)
+
