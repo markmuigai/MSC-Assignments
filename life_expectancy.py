c5b40ea7ac400ea09ee232315d9dc62ee34dc75b
Mark Muigai <markmuigai@gmail.com>
2022-11-13 17:53:01 +0300

    add numpy

--- life_expectancy.py
+++ life_expectancy.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+# from sklearn.model_selection import trai
 
 dataset = pd.read_csv('life_exp.csv');
+print()
 
+#correlation = np.corrcoef(dataset);
+#print(correlation);
