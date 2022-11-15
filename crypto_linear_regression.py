cbb8f04c910a4ee02ea483754bf9875bdffbb743
Mark Muigai <markmuigai@gmail.com>
2022-11-15 18:14:31 +0300

    Stable code

--- crypto_linear_regression.py
+++ crypto_linear_regression.py
@@ -1,9 +1,59 @@
-# Crypto Univariate Linear Regression
+import pandas
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
 
-# Install libraries
+crypot_dataset = pandas.read_csv('cryptocoin_data.csv')
 
-# Read data
 
-# Plot data
+print(crypot_dataset.info())
+print(crypot_dataset.corr())
 
-# ML Model
+x = crypot_dataset['High']
+y = crypot_dataset['Close']
+
+
+def linear_regression(x, y):
+    N = len(x)
+    x_mean = x.mean()
+    y_mean = y.mean()
+
+    B1_num = ((x - x_mean) * (y - y_mean)).sum()
+    B1_den = ((x - x_mean) ** 2).sum()
+    B1 = B1_num / B1_den
+
+    B0 = y_mean - (B1 * x_mean)
+
+    reg_line = 'y = {} + {}β'.format(B0, round(B1, 3))
+
+    return (B0, B1, reg_line)
+
+
+def corr_coef(x, y):
+    N = len(x)
+
+    num = (N * (x * y).sum()) - (x.sum() * y.sum())
+    den = np.sqrt((N * (x * 2).sum() - x.sum() * 2) * (N * (y * 2).sum() - y.sum() * 2))
+    R = num / den
+    return R
+
+
+B0, B1, reg_line = linear_regression(x, y)
+print('Regression Line: ', reg_line)
+R = corr_coef(x, y)
+print('Correlation Coef.: ', R)
+print('"Goodness of Fit": ', R**2)
+
+
+def predict(B0, B1, new_x):
+    y = B0 + B1 * new_x
+    return y
+
+
+new_x = 0.3112
+predict_output = predict(B0, B1, new_x)
+print(new_x)
+# plt.rcParams.update({'figure.figsize':(10,8), 'figure.dpi':100})
+# sns.lmplot(x='High', y='Close', data=crypot_dataset)
+# plt.title("Scatter Plot with Linear fit");
+# plt.show()
--- life_expectancy.py
+++ life_expectancy.py
@@ -1,12 +1,49 @@
-import pandas as pd
-import numpy as np
+import pandas
+import numpy
+import matplotlib.pyplot as plt
 import seaborn as sns
-# from sklearn.model_selection import trai
+from sklearn import model_selection
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-dataset = pd.read_csv('life_exp.csv');
-dataset.drop('Country', inplace=True, axis=1)
-dataset.drop('Status', inplace=True, axis=1)
+dataset = pandas.read_csv('life_exp.csv')
 
-var =np.corrcoef(dataset)
-print(var)
+# Find Correlation
+# plt.figure(figsize= (14,10))
+# sns.heatmap(dataset.corr(), annot=True)
+# plt.show()
 
+print('\n Data shape:  ', dataset.shape)
+print('\n Data Columns:  ', dataset.shape)
+print('\n ', dataset.info())
+print('\n', dataset.isnull().sum(axis=0))
+
+dataset.fillna(value=dataset.mean(), inplace=True)
+print('\n', dataset.isnull().sum(axis=0))
+
+X = dataset[['Total expenditure','GDP']].values
+Y = dataset['Life expectancy'].values
+
+print(X)
+print(X.shape)
+print(Y)
+print(Y.shape)
+
+validation_size = 0.10
+seed = 9
+
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                random_state=seed)
+
+model_LR = LinearRegression()
+model_LR.fit(X_train, Y_train)
+
+#  let the model predict X_validation
+predictions = model_LR.predict(X_validation)
+print('R squared ', r2_score(Y_validation, predictions))
+print("Root Mean Squared", numpy.sqrt(mean_squared_error(Y_validation, predictions)))
+print("Mean Absolute Error", numpy.sqrt(mean_absolute_error(Y_validation, predictions)))
+
+print(Y_validation)
+print(predictions)
